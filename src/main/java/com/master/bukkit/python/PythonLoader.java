ef3b6358f598a7fc460ec1274c29928178581bbe
lahwran <blendmaster1024@gmail.com>
2011-08-26 17:01:30 -0600

    Converted to maven, using shade plugin to include jython jar, so no need to have a manifest.mf with a classpath line. Added jython to repository, as the jython license allows redistribution.

---
+++ src/main/java/com/master/bukkit/python/PythonLoader.java
@@ -0,0 +1,55 @@
+package com.master.bukkit.python;
+
+import java.io.File;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.python.PythonPlugin;
+import org.bukkit.plugin.python.PythonPluginLoader;
+
+public class PythonLoader extends JavaPlugin
+{
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		System.out.println("Enabling Python Plugin loader.");
+		
+		//load all plugin classes that they are managed by the same plugin loader
+		try {
+			Class.forName("org.bukkit.plugin.python.PythonPlugin");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			int i = 1;
+			while(true)
+			{
+				Class.forName("org.bukkit.plugin.python.PythonPluginLoader$"+i);
+				i++;
+			}
+		} catch (ClassNotFoundException e) {
+			
+		}
+		
+		File pluginDir = new File("plugins");
+		if(!pluginDir.exists()) pluginDir.mkdir();
+		
+		getServer().getPluginManager().registerInterface(PythonPluginLoader.class);
+		getServer().getPluginManager().loadPlugins(pluginDir);
+		
+		for(Plugin p : getServer().getPluginManager().getPlugins())
+		{
+			if(p instanceof PythonPlugin)
+			{
+				getServer().getPluginManager().enablePlugin(p);
+			}
+		}
+	}
+	
+}
