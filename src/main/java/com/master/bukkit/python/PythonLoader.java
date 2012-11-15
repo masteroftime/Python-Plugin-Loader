e33a2aaff13a472ab94e1dd3c02590cc7fb824cf
masteroftime <martinfeiler@hotmail.com>
2012-11-15 15:11:32 +0100

    Download jython jar if it is missing

--- src/main/java/com/master/bukkit/python/PythonLoader.java
+++ src/main/java/com/master/bukkit/python/PythonLoader.java
@@ -1,7 +1,13 @@
 package com.master.bukkit.python;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,6 +37,55 @@ public class PythonLoader extends JavaPlugin {
      */
     @Override
     public void onLoad() {
+        //check if jython.jar exists if not try to download
+        if(!new File("lib/jython.jar").exists()) {
+            getServer().getLogger().log(Level.SEVERE, "Could not find lib/jython.jar! I will try to automatically download it for you.");
+            try {
+                URL website = new URL("http://cloud.github.com/downloads/masteroftime/Python-Plugin-Loader/jython.jar");
+                URLConnection connection = website.openConnection();
+                connection.connect();
+
+                //create lib folder if it doesn't exist
+                File lib = new File("lib");
+                if(!lib.exists()) {
+                    lib.mkdir();
+                }
+
+                //delete temporary _dl file if it already exists
+                File dl_file = new File("lib/jython.jar_dl");
+                if(dl_file.exists()) {
+                    dl_file.delete();
+                }
+
+                InputStream in = connection.getInputStream();
+                FileOutputStream out = new FileOutputStream(dl_file);
+
+                long total = connection.getContentLengthLong();
+                long progress = 0;
+                byte[] buffer = new byte[1024];
+                int read;
+                long start = System.currentTimeMillis();
+
+                while((read = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, read);
+                    progress += read;
+                    if(System.currentTimeMillis() - start > 2000) {
+                        System.out.println("Downloading Jython: " + (progress*100/total) + " %");
+                        start = System.currentTimeMillis();
+                    }
+                }
+
+                out.close();
+                in.close();
+
+                dl_file.renameTo(new File("lib/jython.jar"));
+                getServer().getLogger().log(Level.INFO, "Download successful!");
+            } catch (IOException e) {
+                getServer().getLogger().log(Level.SEVERE, "Error while donwloading jython.jar, loading of python plugins will fail! Please download jython from https://github.com/downloads/masteroftime/Python-Plugin-Loader/jython.jar and place it in the lib folder");
+                e.printStackTrace();
+            }
+        }
+
         //System.out.println("PythonLoader: initializing");
         // This must occur as early as possible, and only once.
         PluginManager pm = Bukkit.getServer().getPluginManager();
